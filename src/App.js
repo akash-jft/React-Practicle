83152b615c6fe131f76895c5e92002736b9d33a6
akash-jft <akash.sharma@jellyfishtechnologies.com>
2020-08-08 19:47:54 +0530

    do more practicle

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import Hello from './component/test'
 import Parent from './component/Parentclass'
 import Usergreting from './component/Usergreeting'
 import Namelist from './component/namelist'
-
+import Form from './component/form'
+import LifecycleA from './component/LifecycleA'
+import Fragment from './component/Fragment'
+import Table from './component/Table'
+import Purecomp from './component/Purecomp'
+import Refrsdemo from './component/Refrsdemo'
 function App() {
   return (
     <div className="App">
-   <Namelist/>
+   <Refrsdemo/>
     </div>
   );
 }
